d39a6eaed0d8edad80a0d21ad20b8dd9c8bae3b4
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-02 16:01:56 -0600

    70 - Interpolacao Linear e Movimento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed = 0.03f;
     [SerializeField] private float forwardSpeed = 0.05f;
+
+    [SerializeField] private float targetPositionX;
     private void Update()
     {
         Vector3 targetPosition = transform.position;
-        if (Input.GetKey(KeyCode.A))
-        {
-            //Vector3(-1,0,0)*0.03 = Vector3(-0.03, 0, 0);
-            targetPosition += Vector3.left * horizontalSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            targetPosition += Vector3.right * horizontalSpeed * Time.deltaTime;
-        }
+
+        targetPosition.x = Mathf.Lerp(transform.position.x, targetPositionX, Time.deltaTime * horizontalSpeed);
 
         targetPosition += Vector3.forward * forwardSpeed * Time.deltaTime;
 
