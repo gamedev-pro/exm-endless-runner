1f8a03cf1701c966e5b6f057088c11f5c4feb6e1
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-02 15:31:15 -0600

    60 - Criando seus proprios componentes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Update!");
+    }
+}
