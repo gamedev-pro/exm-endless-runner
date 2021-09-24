26067bb6029e612b35ebe71c7f45d1f35b8bc227
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-24 07:07:14 -0600

    94-2 - Implementando geracao infinita pt 2

--- Assets/Runtime/Scripts/Level/EndlessTrackGenerator.cs
+++ Assets/Runtime/Scripts/Level/EndlessTrackGenerator.cs
@@ -11,12 +11,58 @@ public class EndlessTrackGenerator : MonoBehaviour
     [Space]
     [SerializeField] private int initialTrackCount = 10;
     [SerializeField] private int minTracksInFrontOfPlayer = 3;
+
+    [SerializeField] private float minDistanceToConsiderInsideTrack = 3;
     private List<TrackSegment> currentSegments = new List<TrackSegment>();
 
     private void Start()
     {
         SpawnTrackSegment(firstTrackPrefab, null);
         SpawnTracks(initialTrackCount);
+    }
+
+    private void Update()
+    {
+        //Em qual track o player esta?
+        int playerTrackIndex = FindTrackIndexWithPlayer();
+
+        if (playerTrackIndex < 0)
+        {
+            //TODO: Throw error
+            return;
+        }
+
+        //Instancia tracks a frente do player (se necessario)    
+        int tracksInFrontOfPlayer = currentSegments.Count - (playerTrackIndex + 1);
+        if (tracksInFrontOfPlayer < minTracksInFrontOfPlayer)
+        {
+            SpawnTracks(minTracksInFrontOfPlayer - tracksInFrontOfPlayer);
+        }
+
+        //Remove tracks atras do player
+        for (int i = 0; i < playerTrackIndex; i++)
+        {
+            TrackSegment track = currentSegments[i];
+            //IMPORTANTE: Passar o GameObject
+            Destroy(track.gameObject);
+        }
+        currentSegments.RemoveRange(0, playerTrackIndex);
+    }
+
+    private int FindTrackIndexWithPlayer()
+    {
+        for (int i = 0; i < currentSegments.Count; i++)
+        {
+            TrackSegment track = currentSegments[i];
+
+            if (player.transform.position.z >= (track.Start.position.z + minDistanceToConsiderInsideTrack) &&
+                player.transform.position.z <= track.End.position.z)
+            {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     private void SpawnTracks(int trackCount)
@@ -56,40 +102,6 @@ public class EndlessTrackGenerator : MonoBehaviour
 
         return trackInstance;
     }
-
-    private void Update()
-    {
-        return;
-
-        //Em qual track o player esta?
-        int playerTrackIndex = -1;
-        for (int i = 0; i < currentSegments.Count; i++)
-        {
-            TrackSegment track = currentSegments[i];
-            if (player.transform.position.z >= track.Start.position.z &&
-                player.transform.position.z <= track.End.position.z)
-            {
-                playerTrackIndex = i;
-                break;
-            }
-        }
-
-        if (playerTrackIndex < 0)
-        {
-            //TODO: Throw error
-            return;
-        }
-
-        //Instancia tracks a frente do player (se necessario)    
-        int trackInFrontOfPlayer = currentSegments.Count - (playerTrackIndex + 1);
-        if (trackInFrontOfPlayer < minTracksInFrontOfPlayer)
-        {
-            //Spawn de mais tracks!
-        }
-
-        //Remove tracks atras do player
-    }
-
 }
 
 
