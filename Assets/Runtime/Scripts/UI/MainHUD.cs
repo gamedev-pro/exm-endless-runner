cb5fdb354eb6272854621bb868bf5d4deec8923a
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-26 14:28:43 -0600

    105 - Implementando Botoes

--- Assets/Runtime/Scripts/UI/MainHUD.cs
+++ Assets/Runtime/Scripts/UI/MainHUD.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainHUD : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
     [SerializeField] private TextMeshProUGUI scoreText;
 
+    [SerializeField] private Button testButton;
+
     private void LateUpdate()
     {
         scoreText.text = $"Score : {player.Score}";
+    }
+
+    public void TestButton()
+    {
+        testButton.interactable = false;
+        Debug.Log("Clicked!");
     }
 }
