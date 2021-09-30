7b8d449831a3b1ec07d97923815f5ba851d10fe8
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-30 08:09:04 -0600

    114-2 - Feedback de colisao

--- Assets/Runtime/Scripts/Level/ObstacleDecoration.cs
+++ Assets/Runtime/Scripts/Level/ObstacleDecoration.cs
@@ -11,16 +11,6 @@ public class ObstacleDecoration : MonoBehaviour
     private AudioSource audioSource;
     private AudioSource AudioSource => audioSource == null ? audioSource = GetComponent<AudioSource>() : audioSource;
 
-    //Animation Clip feedback
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            PlayCollisionFeedback();
-        }
-    }
-
     public void PlayCollisionFeedback()
     {
         AudioUtility.PlayAudioCue(AudioSource, collisionAudio);
